70dff82475889024bb2a7ae04d8c4deb3f9d49c7
NightHao <48900334+NightHao@users.noreply.github.com>
2021-06-10 18:52:30 +0800

    Add files via upload

--- Sort/QuickSort.java
+++ Sort/QuickSort.java
@@ -6,7 +6,7 @@ public class QuickSort extends Sort{
         s1 = "快速排序（英語：Quicksort），又稱分割區交換排序（partition-exchange sort，";
         s2 = "簡稱快排，一種排序演算法，最早由東尼·霍爾提出。在平均狀況下，排序n個項目要O(n log n)次比較。";
         s3 = "在最壞狀況下則需要O(n^2)次比較，但這種狀況並不常見。事實上，";
-        s4 = "快速排序Theta(nlogn)通常明顯比其他演算法更快，";
+        s4 = "快速排序Theta(n log n)通常明顯比其他演算法更快，";
         s5 = "因為它的內部迴圈（inner loop）可以在大部分的架構上很有效率地達成。";
         return String.format("%s\n%s\n%s\n%s\n%s\n",s1,s2,s3,s4,s5);
     }
@@ -39,19 +39,23 @@ public class QuickSort extends Sort{
 		for (int j = left; j < right; j++) {
 			if (number[j] <= number[right]) {
 				i++;
-				swap(number, i, j);
+				int temp = number[i];
+		        number[i] = number[j];
+		        number[j] = temp;
                 count++;
 			}
 		}
-		swap(number, i + 1, right);
+        int temp = number[i + 1];
+        number[i + 1] = number[right];
+        number[right] = temp;
         count++;
 		return i + 1;
 	}
 
-	private void swap(int[] data, int i, int j) {
+	/*private void swap(int[] data, int i, int j) {
 		int temp = data[i];
 		data[i] = data[j];
 		data[j] = temp;
-	}
+	}*/
 
 }
