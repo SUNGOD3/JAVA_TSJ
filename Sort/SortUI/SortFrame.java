2da62aa9339643381ce63eff42413212dab952d2
SUNGOD3 <72863539+SUNGOD3@users.noreply.github.com>
2021-06-12 18:47:35 +0800

    Add files via upload

--- Sort/SortUI/SortFrame.java
+++ Sort/SortUI/SortFrame.java
@@ -10,6 +10,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+
 import javax.swing.JComboBox;
 
 public class SortFrame extends JFrame {
@@ -17,11 +18,12 @@ public class SortFrame extends JFrame {
 	private final JComboBox<String> imagesJComboBox,imagesJComboBox2; // hold icon names
 	private static final String[] names = {"BubbleSort","InsertionSort","MergeSort","QuickSort","SelectionSort"};
 	private static final String[] names2 = {"RandomArray","ExampleArray","ReadArray","InputArray"};
-	private JButton increaseButton,decreaseButton,generateArray; // button to decrease font size
+	private String T1,T2;// 捕捉combobox的狀態
+	private JButton increaseButton,decreaseButton,generateArray,RunButton; // button to decrease font size
 	private JTextArea text; // displays example text
 	private int fontSize = 20; // current font size
 	private newArray Array;
-	private Sort chooseSort;
+	private Sort chooseSort;// TO DO
 
 	public SortFrame() {
 		super("Sort Frame Test");
@@ -29,36 +31,20 @@ public class SortFrame extends JFrame {
 		imagesJComboBox2 = new JComboBox<String>(names2); // set up JComboBox
       	imagesJComboBox.setMaximumRowCount(5); // display three rows
 		imagesJComboBox2.setMaximumRowCount(4); // display three rows
-		imagesJComboBox.addItemListener(
-			new ItemListener(){ // anonymous inner class
-				// handle JComboBox event
-				@Override
-				public void itemStateChanged(ItemEvent event){
-					// determine whether item selected
-					if (event.getStateChange() == ItemEvent.SELECTED){
-							System.out.println(event.getItem());
-					}
-				} 
-			} // end anonymous inner class
-      	); 
-		imagesJComboBox2.addItemListener(
-			new ItemListener(){ // anonymous inner class
-				// handle JComboBox event
-				@Override
-				public void itemStateChanged(ItemEvent event){
-					// determine whether item selected
-					if (event.getStateChange() == ItemEvent.SELECTED){
-							System.out.println(event.getItem());
-					}
-				} 
-			} // end anonymous inner class
-      	); 
+		ItemListener handler1 = new EventListner();
+		ItemListener handler2 = new EventListner();
+		imagesJComboBox.addItemListener(handler1); 
+		imagesJComboBox2.addItemListener(handler2); 
+		imagesJComboBox.setSelectedIndex(1); // 改變選項時才偵測,且一開始為0
+		imagesJComboBox2.setSelectedIndex(2);// 直接改預設值先讓其跑一次
 		EventListner handler = new EventListner();
 		// create buttons and add action listeners
 		generateArray = new JButton("Generate Array");
+		RunButton = new JButton("Go Go!");
 		decreaseButton = new JButton("Decrease font size");
 		increaseButton = new JButton("Increase font size");
 		generateArray.addActionListener(handler);
+		RunButton.addActionListener(handler);
 		decreaseButton.addActionListener(handler);
 		increaseButton.addActionListener(handler);
 
@@ -69,6 +55,7 @@ public class SortFrame extends JFrame {
 		// add GUI components to frame
 		JPanel panel = new JPanel(); // used to get proper layout
 		panel.add(generateArray);
+		panel.add(RunButton);
 		panel.add(increaseButton);
 		panel.add(decreaseButton);
 		panel.add(imagesJComboBox); // add combobox to JFrame
@@ -77,9 +64,29 @@ public class SortFrame extends JFrame {
 		add(new JScrollPane(text)); // allow scrolling
 	}
 
-	private class EventListner implements ActionListener {
-		public void actionPerformed(ActionEvent e){
-			if(e.getSource()==generateArray){//參考generateRandomArrayTest
+	private class EventListner implements ActionListener,ItemListener {
+		public void judgeSortType(){
+			System.out.println("You choose "+T1);
+			if(T1=="BubbleSort"){
+
+			}
+			else if(T1=="InsertionSort"){
+
+			}
+			else if(T1=="MergeSort"){
+
+			}
+			else if(T1=="QuickSort"){
+
+			}
+			else if(T1=="SelectionSort"){
+
+			}
+		}
+		public void judgeInput(){
+			System.out.println("You choose "+T2);
+			if(T2=="RandomArray"){
+				//參考generateRandomArrayTest
 				String sn=JOptionPane.showInputDialog("Input the size of your array");
 				String slr=JOptionPane.showInputDialog("Input the lower bound of your array");
 				String sur=JOptionPane.showInputDialog("Input the upper bound of your array");
@@ -87,8 +94,31 @@ public class SortFrame extends JFrame {
 				int lr=Integer.parseInt(slr);
 				int ur=Integer.parseInt(sur);
 				generateRandomArray tmp = new  generateRandomArray(n,lr,ur);
+				Array = new newArray(tmp.getArray(),lr,ur);
+				text.setText(Array.toString());
+			}
+			else if(T2=="ExampleArray"){
+				generateExampleArray tmp = new generateExampleArray();
 				Array = new newArray(tmp.getArray());
 				text.setText(Array.toString());
+			}
+			else if(T2=="ReadArray"){
+				findFile fd = new findFile();
+				System.out.println(fd.getName());
+				ReadArray tmp = new ReadArray(fd.getName()); 
+				Array = new newArray(tmp.run());
+				text.setText(Array.toString());
+			}
+			else if(T2=="InputArray"){
+				//TO DO...
+			}
+		}
+		public void actionPerformed(ActionEvent e){//當Button被按到時
+			if(e.getSource()==generateArray){//產生陣列
+				judgeInput();
+			}
+			if(e.getSource()==RunButton){//Sort
+				judgeSortType();
 			}
 			//以下參考作業
 			if(e.getSource()==increaseButton){
@@ -110,5 +140,23 @@ public class SortFrame extends JFrame {
 				}
 			}
 		}
+
+		@Override//捕捉combobox
+		public void itemStateChanged(ItemEvent e){
+			// determine whether item selected
+			if(e.getSource()==imagesJComboBox){
+				if (e.getStateChange() == ItemEvent.SELECTED){
+					System.out.println(e.getItem());
+					T1 = (String) e.getItem();
+				}
+			}
+			else if(e.getSource()==imagesJComboBox2){
+				if (e.getStateChange() == ItemEvent.SELECTED){
+					System.out.println(e.getItem());
+					T2 = (String) e.getItem();
+				}
+			}
+			
+		} 
 	}
 }
