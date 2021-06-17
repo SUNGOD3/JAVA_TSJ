8e8fb81894559d82cc3d3dd163308e8cc33b456c
SUNGOD3 <72863539+SUNGOD3@users.noreply.github.com>
2021-06-18 04:07:39 +0800

    Delete generate2DExampleArrayTest.java

--- 2D_matrix_multiplication/generate2DExampleArrayTest.java
+++
@@ -1,7 +0,0 @@
-public class generate2DExampleArrayTest {
-    public static void main(String[] args) {
-        generate2DExampleArray Rx = new generate2DExampleArray();
-        new2DArray Ra2 = new new2DArray(Rx.getArray());
-        System.out.println(Ra2.toString());
-    }
-}
