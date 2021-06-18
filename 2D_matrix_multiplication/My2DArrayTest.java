50a475c7645ee5f97930dc667bb3155806ed1150
GoGoKuan <84001572+GoGoKuan@users.noreply.github.com>
2021-06-18 13:41:31 +0800

    Add files via upload

--- 2D_matrix_multiplication/My2DArrayTest.java
+++ 2D_matrix_multiplication/My2DArrayTest.java
@@ -2,7 +2,7 @@ public class My2DArrayTest{
     //之後會整合成SortAPI
 	public static void main(String[] args) {
         int [][] tmp = {{1,50,66,-11,5}, {5,6,11,20,8}};
-        String [] tmp2 = {"1  5 -5 66","15 66  52 88"};
+        String [] tmp2 = {"1 5 -5 66","15 66 52 88"};
         new2DArray Ra2 = new new2DArray(tmp);
         new2DArray Ra3 = new new2DArray(tmp, -12, 67);
         new2DArray Ra4 = new new2DArray(tmp2);
@@ -10,7 +10,10 @@ public class My2DArrayTest{
         System.out.println(Ra3.toString());
         System.out.println(Ra4.toString());
         generate2DExampleArray Rx = new generate2DExampleArray();
+        generate2DRandomArray Ry = new generate2DRandomArray(20, 55, -200, 600);
         new2DArray Ra5 = new new2DArray(Rx.getArray());
+        new2DArray Ra6 = new new2DArray(Ry.getArray());
         System.out.println(Ra5.toString());
+        System.out.println(Ra6.toString());
     }
 }
