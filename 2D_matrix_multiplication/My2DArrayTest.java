3a24a12fe16eb7d1efba5ec4cdb693a44d453101
NightHao <48900334+NightHao@users.noreply.github.com>
2021-06-20 14:05:58 +0800

    Add files via upload

--- 2D_matrix_multiplication/My2DArrayTest.java
+++ 2D_matrix_multiplication/My2DArrayTest.java
@@ -19,10 +19,13 @@ public class My2DArrayTest{
         System.out.println(Ra6.toString());
         Mul Test1 = new StrassenMul();
         Mul Test2 = new NaiveMul();
+        Mul Test3 = new SparseMul();
         int [][] testA = {{1,2,3,4},{-2,4,5,6},{5,9,8,7},{2,8,6,9}},testB = {{5,6,-1,5},{0,5,1,1},{10,7,6,3},{5,6,7,8}};
+        int [][] testSA = {{1,0,0},{-1,0,3}}, testSB = {{7,0,0},{0,0,0},{0,0,1}};
         //Strassen此演算法只能接受長度相同且為2的覓次的方陣相乘(實際上可以用一些技巧解決這個問題，如果你專題有興趣做這個的話)
-        Test1.MulMain(testA, testB);
-        Test2.MulMain(testA, testB);
+        Test1.MulMain(testSA, testSB);
+        //Test2.MulMain(testSA, testSB);
+        Test3.MulMain(testSA, testSB);
         //若不確定答案正確性可前往以下網頁
         //https://matrixcalc.org/zh/#%7B%7B1,2,3,4%7D,%7B-2,4,5,6%7D,%7B5,9,8,7%7D,%7B2,8,6,9%7D%7D%2a%7B%7B5,6,-1,5%7D,%7B0,5,1,1%7D,%7B10,7,6,3%7D,%7B5,6,7,8%7D%7D
     }
